ddd1a683f5498220204c692afab3b41cee538d70
luiggidev <luiggibellincanta@gmail.com>
2022-01-15 19:17:35 -0300

    Multiple script changes, fixed delay for bell, timer rendering correctly, logic tested for interval.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
 const shortBell = new Audio("bells/shortBell.mp3");
 const longBell = new Audio("bells/longBell.mp3");
+const timerContent = document.getElementById("timer");
 
 function pauseBell() {
   shortBell.pause();
+  clearInterval(myInterval);
+  timerContent.innerHTML = '';
 }
 
 function setFutureTime() {
-  timeToAddInMinutes = 15;
-  timeToAddInSeconds = 6;
+  // timeToAddInMinutes = 15;
+  timeToAddInSeconds = 7;
   future = new Date();
   // future.setMinutes( future.getMinutes() + timeToAddInMinutes );
   future.setSeconds( future.getSeconds() + timeToAddInSeconds );
 }
 
 function firstStartBell() {
-  console.log('firstStartBell ran')
-  startBell();
+  console.log('firstStartBell ran');
   myInterval = setInterval('updateTimer()', 1000);
-  setFutureTime()
+  setFutureTime();
+  setTimeout(startBell, 1000);
+  // startBell();
 }
 
 function startBell() {
   console.log('startBell ran')
   shortBell.volume = 0.1;
-
   shortBell.play();
 }
 
 function updateTimer() {
+  console.log(updateTimer);
   now = new Date();
   diff = future - now;
 
@@ -41,19 +45,17 @@ function updateTimer() {
   m = mins - hours * 60;
   s = secs - mins * 60;
 
-  document.getElementById("timer")
-  .innerHTML =
-  '<div>' + m + '<span>:</span>' + s + '</div>';
+  const timerZero = '<div class="countdown-text font-size text-bold">Remaining time</div>';
+  let timerDynamic = '<div class="countdown-text font-size text-bold">Remaining time</div><div class="countdown-timer text-bold">' + m + '<span>:</span>' + s + '</div>';
+  
+  timerContent.innerHTML = timerDynamic;
 
-  // If minutes reaches 0.
-  if(m>=59){
-    clearInterval(myInterval);
+  // If minute loops over and is higher then 59 startbell and reset time.
+  // ! Add restriction, user cant add a number higher then 59 in this scenario.
+  if( m >= 59 ){
+    timerContent.innerHTML = timerZero;
 
-    document.getElementById("timer")
-    .innerHTML =
-    '<div>' + 0 + '<span>:</span>' + 0 + '</div>';
-
+    setFutureTime();
     startBell();
-    setFutureTime()
   }
 }
