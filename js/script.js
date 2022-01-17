a1dc5f8d9ead2b3e377b6b8cf01692d7dde75137
luiggidev <luiggibellincanta@gmail.com>
2022-01-17 10:32:43 -0300

    Enable debug mode and logFunctionName function

--- js/script.js
+++ js/script.js
@@ -2,36 +2,55 @@ const shortBell = new Audio("bells/shortBell.mp3");
 const longBell = new Audio("bells/longBell.mp3");
 const timerContent = document.getElementById("timer");
 
+// let isBellEnabled = true;
+// let shortLoaded = false;
+// let LongLoaded = false;
+let isdebug = true;
+
+function logFunctionName(arguments) {
+  let functionName = (arguments) ? 'Function: ' + arguments.callee.name + '() ran' : 'Error, logFunctionName() arguments not present';
+  console.log(functionName);
+}
+
+function setFutureTime() {
+  timeToAddInMinutes = 15;
+  timeToAddInSeconds = 7;
+  future = new Date();
+  future.setMinutes( future.getMinutes() + timeToAddInMinutes );
+  // future.setSeconds( future.getSeconds() + timeToAddInSeconds );
+}
+
+function strikeBell() {
+  if (isdebug){
+    logFunctionName(arguments);
+    console.log(shortBell.duration);
+  }
+  shortBell.volume = 0.1;
+  shortBell.play();
+}
+
+function startBell() {
+  if (isdebug){
+    logFunctionName(arguments);
+  }
+  setFutureTime();
+  myInterval = setInterval('updateTimer()', 1000);
+  setTimeout(strikeBell, 1000);
+}
+
 function pauseBell() {
+  if (isdebug){
+    logFunctionName(arguments);
+  }
   shortBell.pause();
   clearInterval(myInterval);
   timerContent.innerHTML = '';
 }
 
-function setFutureTime() {
-  // timeToAddInMinutes = 15;
-  timeToAddInSeconds = 7;
-  future = new Date();
-  // future.setMinutes( future.getMinutes() + timeToAddInMinutes );
-  future.setSeconds( future.getSeconds() + timeToAddInSeconds );
-}
-
-function firstStartBell() {
-  console.log('firstStartBell ran');
-  myInterval = setInterval('updateTimer()', 1000);
-  setFutureTime();
-  setTimeout(startBell, 1000);
-  // startBell();
-}
-
-function startBell() {
-  console.log('startBell ran')
-  shortBell.volume = 0.1;
-  shortBell.play();
-}
-
 function updateTimer() {
-  console.log(updateTimer);
+  if (isdebug){
+    logFunctionName(arguments);
+  }
   now = new Date();
   diff = future - now;
 
@@ -50,12 +69,11 @@ function updateTimer() {
   
   timerContent.innerHTML = timerDynamic;
 
-  // If minute loops over and is higher then 59 startbell and reset time.
+  // If minute loops over and is higher then 59 strikeBell and reset time.
   // ! Add restriction, user cant add a number higher then 59 in this scenario.
   if( m >= 59 ){
     timerContent.innerHTML = timerZero;
-
     setFutureTime();
-    startBell();
+    strikeBell();
   }
 }
