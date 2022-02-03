82c6f813f222c5320601935d6ea77d2443e8a81e
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2022-02-03 19:11:30 -0300

    changes img to svg to gain fill control

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,19 +4,19 @@ import ProgressBar from './ProgressBar';
 class Buttons extends Component {
     render() {
         return (
-            <div>
-                <div className="row bell-buttons">
-                    <button onClick={this.props.handleButtonClick}>
-                        {this.props.buttonState ? 
-                            <img src="/images/bell.svg" alt="Bell" className="bell" /> : 
-                            <img src="/images/bell-slash.svg" alt="Bell slash"className="bell-slash"/>
-                        }
-                    </button>
-                    
+                <div>
+                    <div className="row bell-buttons">
+                        <button onClick={this.props.handleButtonClick}>
+                            {this.props.buttonState ? 
+                                <svg class="bell" width="80px" height="80px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12 1C8.318 1 5 3.565 5 7v4.539a3.25 3.25 0 01-.546 1.803l-2.2 3.299A1.518 1.518 0 003.519 19H8.5a3.5 3.5 0 107 0h4.982a1.518 1.518 0 001.263-2.36l-2.2-3.298A3.25 3.25 0 0119 11.539V7c0-3.435-3.319-6-7-6zM6.5 7c0-2.364 2.383-4.5 5.5-4.5s5.5 2.136 5.5 4.5v4.539c0 .938.278 1.854.798 2.635l2.199 3.299a.017.017 0 01.003.01l-.001.006-.004.006-.006.004-.007.001H3.518l-.007-.001-.006-.004-.004-.006-.001-.007.003-.01 2.2-3.298a4.75 4.75 0 00.797-2.635V7zM14 19h-4a2 2 0 104 0z"/></svg> : 
+                                <svg class="bell-slash" width="80px" height="80px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1.22 1.22a.75.75 0 011.06 0l20.5 20.5a.75.75 0 11-1.06 1.06L17.94 19H15.5a3.5 3.5 0 11-7 0H3.518a1.518 1.518 0 01-1.263-2.36l2.2-3.298A3.25 3.25 0 005 11.539V7c0-.294.025-.583.073-.866L1.22 2.28a.75.75 0 010-1.06zM10 19a2 2 0 104 0h-4zM6.5 7.56l9.94 9.94H3.517l-.007-.001-.006-.004-.004-.006-.001-.007.003-.01 2.2-3.298a4.75 4.75 0 00.797-2.635V7.56z"/><path d="M12 2.5c-1.463 0-2.8.485-3.788 1.257l-.04.032a.75.75 0 11-.935-1.173l.05-.04C8.548 1.59 10.212 1 12 1c3.681 0 7 2.565 7 6v4.539c0 .642.19 1.269.546 1.803l1.328 1.992a.75.75 0 11-1.248.832l-1.328-1.992a4.75 4.75 0 01-.798-2.635V7c0-2.364-2.383-4.5-5.5-4.5z"/></svg>
+                            }
+                        </button>
+                        
+                    </div>
+                    <div id="timer"></div>
+                    {/* <ProgressBar /> */}
                 </div>
-                <div id="timer"></div>
-                {/* <ProgressBar /> */}
-            </div>
         );
     }
 }
