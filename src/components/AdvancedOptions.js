58ad84a23df0c969648b999f91cdb19f3d6cb1a3
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2022-02-03 19:10:58 -0300

    Advanced options

--- src/components/AdvancedOptions.js
+++ src/components/AdvancedOptions.js
@@ -4,14 +4,24 @@ import './AdvancedOptions.css';
 class AdvancedOptions extends Component {
       render() {
         return (
-        <div className="adv-options"> 
-          <div>Advanced Options:</div>
-          <form onSubmit={this.props.handleSubmit}>
-            <label>
-              Minutes Between Bells: <input type="number" value={this.props.intervalValue} onChange={this.props.handleChange} />
-            </label>
-          </form> 
-        </div>
+          <div>
+            <div className="row adv-button">
+                        <button onClick={this.props.handleButtonClick}>
+                            {this.props.buttonState ? 
+                                <img src="/images/settings.svg" alt="Bell" className="settings-button" /> : 
+                                <img src="/images/settings.svg" alt="Bell slash"className="settings-button-slash"/>
+                            }
+                        </button>
+                    </div>
+            <div className="adv-options"> 
+              <div>Advanced Options:</div>
+              <form onSubmit={this.props.handleSubmit}>
+                <label>
+                  Minutes Between Bells: <input type="number" value={this.props.intervalValue} onChange={this.props.handleChange} />
+                </label>
+              </form> 
+            </div>
+          </div>
         );
       }
     }
