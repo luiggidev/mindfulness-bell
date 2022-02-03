c08812880a18eee880b0ad42d3a5ad3dde1f3b97
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2022-02-03 19:49:24 -0300

    added remaining time

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '3'
+      value: '60'
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -8,7 +8,8 @@ class Timer extends Component {
             title: 'Timer component',
             date: new Date(),
             futureDate: new Date(),
-            isBellEnabled: false
+            isBellEnabled: false,
+            Remaining: ''
         };
     }
     
@@ -57,7 +58,6 @@ class Timer extends Component {
         if(this.isDebug) {
             console.log('strikeBell');
         }
-
         this.bell.play();
         this.setFutureTime();
     }
@@ -67,11 +67,29 @@ class Timer extends Component {
             console.log('setFutureTime');
         }
         const future = new Date();
-        // future.setMinutes( future.getMinutes() + parseInt(this.props.intervalValue, 10) )
-        future.setSeconds( future.getSeconds() + parseInt(this.props.intervalValue, 10) )
+        future.setMinutes( future.getMinutes() + parseInt(this.props.intervalValue, 10) )
+        // future.setSeconds( future.getSeconds() + parseInt(this.props.intervalValue, 10) )
 
         this.setState({
             futureDate: future
+        });
+    }
+
+    minTwoDigits(n) {
+        return (n < 10 ? '0' : '') + n;
+    }
+
+    timeRemaining(now, future) {
+        if(this.isDebug) {
+            console.log('timeRemaining');
+        }
+        var remain = future - now;
+        var minutes = Math.floor(remain/(1000*60));
+        var seconds = Math.floor((remain/1000) - 60*minutes);
+
+        var remainString = `Next strike: ${this.minTwoDigits(minutes)}:${this.minTwoDigits(seconds)}`;
+        this.setState({
+            remaining: remainString
         });
     }
 
@@ -98,20 +116,39 @@ class Timer extends Component {
         if( this.state.isBellEnabled && this.state.date > this.state.futureDate) {
             this.strikeBell()
         }
+
+        this.timeRemaining(this.state.date, this.state.futureDate)
     }
 
     render() {
         return(
-            <div>
+            <div className="timer-wrapper">
                 <Buttons buttonState={this.state.isBellEnabled} handleButtonClick={this.handleButtonClick}/>
                 {this.isDebug &&
                     <div className="font-size-small">
+
                         <strong>Debug mode: {this.state.title} </strong>
                         <div>Current date: {this.state.date.toLocaleTimeString()}</div>
-                        {this.state.isBellEnabled && <div>Next Strike at: {this.state.futureDate.toLocaleTimeString()}</div> }
-                        {!this.state.isBellEnabled && <div>Bell disabled</div> }
-                        
+
+                        { this.state.isBellEnabled && 
+                                <div>
+                                    Next Strike at: {this.state.futureDate.toLocaleTimeString()}
+                                </div> 
+                                
+                        }
+
+                        { !this.state.isBellEnabled && 
+                                <div>
+                                    Bell disabled
+                                </div> 
+                        }
                     </div>
+                }
+                
+                { this.state.isBellEnabled && 
+                    <div>
+                        {this.state.remaining}
+                    </div>   
                 }
             </div>
         );
