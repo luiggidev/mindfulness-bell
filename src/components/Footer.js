b37270c15d700f6be7e811f9a2981a417a857c30
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2022-02-09 15:34:54 -0300

    Style footer wth tailwindCSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <footer className="col font-size-small">
-                <div>
-                    Made with ☸️ by <a href="https://github.com/luiggidev">Luiggi Bellincanta</a>
+            <footer className="flex justify-center text-base">
+                <div className='absolute bottom-6'>
+                    Made with ☸️ by <a className="underline" href="https://github.com/luiggidev">Luiggi Bellincanta</a>
                 </div>
             </footer>
         );
